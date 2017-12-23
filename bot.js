2ca7db706ea8913f1a4477a7458a8018a9e073e8
JuanTopia <34786576+JuanTopia@users.noreply.github.com>
2017-12-23 15:30:58 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,8 @@ client.on('message', message => {
     if (message.content === '!download') {
     	message.reply('lmao kid go to JuanHax Channel For Downloads');
   	}
+      if (message.content === '!report') {
+    	message.reply('Private Message Me And ScreenShot If He/She Doing Something Against The Rules');
 });
 
 // THIS  MUST  BE  THIS  WAY
