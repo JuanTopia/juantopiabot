0a337ed234f4d51aaf5c67c911ae714494815184
JuanTopia <34786576+JuanTopia@users.noreply.github.com>
2017-12-23 15:13:49 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('Bot Its Online!');
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
@@ -10,5 +10,6 @@ client.on('message', message => {
     	message.reply('pong');
   	}
 });
-//Dont Hack Pls
-client.login(process.env.Mzk0MDEyMDc4NTU1MjY3MDg0.DR-Rlg.R9SzdqTskfrhsSefmQeeGynBA74);
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
