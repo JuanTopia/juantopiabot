72f0ecd7e9315c905b81dc52ac864f736a60b3a0
JuanTopia <34786576+JuanTopia@users.noreply.github.com>
2017-12-23 15:24:48 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '!download') {
+    	message.reply('lmao kid go to JuanHax Channel For Downloads');
   	}
 });
 
